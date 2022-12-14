3dea59317bca8a334bc72ff1ab9cd28cb63f8f7c
Noe Benjamin Reynoso <nreynoso@geniuscode.mx>
2022-12-14 09:43:02 -0700

    Modificacion de tercer EJERCICIO3.kt

--- src/main/kotlin/EJERCICIO3.kt
+++ src/main/kotlin/EJERCICIO3.kt
@@ -1,17 +1,24 @@
 fun isVersionActualSuperior(actualVersion:String,versionToCompare:String):Boolean{
-    //Convertimos ambos parametros a listas, cada elemento es separado por un punto, ademas utilizando una funcion lambda convertimos cada elemento de la lista a entero
-    val actualVersionList  = actualVersion.split('.').map { it.toInt() }
-    val versionToCompareList = versionToCompare.split('.').map { it.toInt() }
-    return when{
+        //Convertimos ambos parametros a listas, cada elemento es separado por un punto, ademas utilizando una funcion lambda convertimos cada elemento de la lista a entero
+        val actualVersionList  = actualVersion.split('.').map { it.toInt() }
+        val versionToCompareList = versionToCompare.split('.').map { it.toInt() }
+
         //verificamos que el primer elemento de la lista de la version actual es mayor
-        actualVersionList[0] > versionToCompareList[0] -> true
+        val expression1 = actualVersionList[0] > versionToCompareList[0]
+
         //verificamos que el primer elemento de ambas listas son iguales ademas de que el segundo elemento de la version actual sea mayor
-        actualVersionList[0] == versionToCompareList[0]  && actualVersionList[1] > versionToCompareList[1]-> true
+        val expression2 = actualVersionList[0] == versionToCompareList[0]  && actualVersionList[1] > versionToCompareList[1]
+
         //verificamos que los primeros dos elementos de ambas listas son iguales y que el ultimo elemento de la version actual sea mayor
-        actualVersionList[0] == versionToCompareList[0]  && actualVersionList[1] == versionToCompareList[1] && actualVersionList[2] >= versionToCompareList[2]-> true
-        //si no se cumplen ninguno de estas condicionales retorna falso
-        else -> false
-    }
+        val expression3 = actualVersionList[0] == versionToCompareList[0]  && actualVersionList[1] == versionToCompareList[1] && actualVersionList[2] >= versionToCompareList[2]
+
+        return when{
+            expression1 -> true
+            expression2 -> true
+            expression3 -> true
+            //si no se cumplen ninguno de estas condicionales retorna falso
+            else -> false
+        }
 }
 fun main(){
     println(isVersionActualSuperior("1.10.1", "1.9.0"))
@@ -19,6 +26,7 @@ fun main(){
     println(isVersionActualSuperior("1.10.1", "1.12.0"))
     println(isVersionActualSuperior("1.10.1", "1.10.2"))
     println(isVersionActualSuperior("1.10.1", "2.0.0"))
+    println(isVersionActualSuperior("1.13.1", "1.10.7"))
 }
 
 /*
